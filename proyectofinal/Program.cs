f9e5f54c13f8e8d4b38c5ef1944a322ff493b9cf
Ruben <destego@gmail.com>
2024-01-14 14:21:20 -0300

    Primeros Pasos

---
+++ proyectofinal/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace proyectofinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+
+}
